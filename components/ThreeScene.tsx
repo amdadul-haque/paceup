77206e1db4a3a1d328f022c0488ca133e411d34c
Majumder99 <souravmajumderk99@gmail.com>
2024-09-16 20:38:45 +0600

    showing the model

--- components/ThreeScene.tsx
+++ components/ThreeScene.tsx
@@ -1,14 +1,11 @@
-'use client';
-import React, { Suspense } from 'react';
-import { Canvas, extend } from '@react-three/fiber';
-import { OrthographicCamera, useGLTF } from '@react-three/drei';
-import { PlaneBufferGeometry } from 'three';
+"use client";
 
-// Extend Three.js geometries so R3F can use them declaratively
-extend({ PlaneBufferGeometry });
+import { useGLTF } from "@react-three/drei";
+import { Canvas } from "@react-three/fiber";
+import { Suspense } from "react";
 
 const Model = () => {
-  const { scene } = useGLTF('/model.glb');
+  const { scene } = useGLTF("/model.glb");
   return <primitive object={scene} />;
 };
 
@@ -17,13 +14,13 @@ const ThreeScene: React.FC = () => {
     <Canvas
       orthographic
       camera={{ zoom: 50, position: [0, 10, 0] }}
-      style={{ height: '100vh', backgroundColor: '#effd80' }}
+      style={{ height: "100vh", backgroundColor: "#effd80" }}
     >
       <ambientLight intensity={0.5} />
-      <Suspense fallback={<span>Loading...</span>}>
+      <Suspense>
         <Model />
         <mesh rotation={[-Math.PI / 2, 0, 0]} receiveShadow>
-          <planeBufferGeometry args={[100, 100]} />
+          {/* <planeBufferGeometry args={[100, 100]} /> */}
           <meshStandardMaterial color="#effd80" />
         </mesh>
       </Suspense>
